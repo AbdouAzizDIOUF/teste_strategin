35a8bd7fabfc30731a1f6114434ce6372c64d5d0
abdouazizdiouf <abdouazizdiouf75@gmail.com>
2021-03-14 23:56:25 +0100

    second question

--- teste.js
+++ teste.js
@@ -9,3 +9,18 @@ function max2number(number1, number2){
     alert ((number1 > number2) ? number1 : number2);
     //return (number1 > number2) ? number1 : number2
 }
+
+/**
+ * This function returns a value according to a criterion
+ *
+ * @param number
+ * @returns {string|*}
+ */
+function fozz_bezz(number){
+    if ((number % 5 === 0) && (number % 3 === 0)) alert("<<FozzBezz>>"); //return "<<FozzBezz>>"
+    if (number % 3 === 0) alert("<<Fozz>>"); //return "<<Fozz>>"
+    if (number % 5 === 0) alert("<<Bezz>>");  //return "<<Bezz>>";
+
+    alert(number);
+    //return number;
+}
