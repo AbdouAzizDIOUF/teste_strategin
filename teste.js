bf6a837baf0b9e0829cf4f4fae83c18f810c1d5a
abdouazizdiouf <abdouazizdiouf75@gmail.com>
2021-03-14 23:55:44 +0100

    first question

---
+++ teste.js
@@ -0,0 +1,11 @@
+/**
+ * returns the maximum between the two numbers passed in parameter
+ *
+ * @param number1
+ * @param number2
+ * @returns {*}
+ */
+function max2number(number1, number2){
+    alert ((number1 > number2) ? number1 : number2);
+    //return (number1 > number2) ? number1 : number2
+}
